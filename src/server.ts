24e9b9a9d07b217e59b294d28295443a5c657ceb
jul1ana <julianaadt97@gmail.com>
2023-09-15 23:21:56 -0300

    add route create-transcription

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
 
 const app = fastify();
 
 // routes
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
 
 app
   .listen({
